2963e931e9b4d9b22a0e230026abe44bdb52b8bd
ShivamX4RABET <shivamguptarooting@gmail.com>
2025-07-15 16:03:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+ADMIN_IDS = [123456789]  # Replace with your Telegram user ID
+WORD_LIMIT = 1000        # Word limit for non-admins
+BOT_TOKEN = "7996000553:AAEIML1rCmQI7zFEQ1SLwrtm8Evmh_I1hrQ"  # Get it from BotFather
